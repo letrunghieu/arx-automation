eb1cae6951b5d3dec785879f22c1599e96429b76
Hieu Le <letrunghieu.cse09@gmail.com>
2020-06-11 23:07:13 +0700

    Targeting Java 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,9 +26,9 @@ tasks.create<JavaExec>("demoPublisher") {
 
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "11"
 }
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "11"
 }
