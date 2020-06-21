1aec215fdbbb6cbbde820dc72ba66a3799ca6a3e
Hieu Le <letrunghieu.cse09@gmail.com>
2020-06-21 11:27:33 +0700

    Fix the Kotlin script for DemoPublisher.kt

--- src/main/kotlin/info/hieule/arx_automation/app/DemoPublisher.kt
+++ src/main/kotlin/info/hieule/arx_automation/app/DemoPublisher.kt
@@ -1,22 +1,21 @@
+@file:JvmName("DemoPublisher")
+
 package info.hieule.arx_automation.app
 
 import com.rabbitmq.client.ConnectionFactory
 
-object DemoPublisher {
-    private const val QUEUE_NAME = "datasets"
+private const val QUEUE_NAME = "dataset"
 
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val connectionFactory = ConnectionFactory()
-        connectionFactory.host = "localhost"
-        connectionFactory.connectionTimeout = 3000
-        connectionFactory.newConnection().use { connection ->
-            connection.createChannel().use { channel ->
-                channel.queueDeclare(QUEUE_NAME, false, false, false, null)
-                val message = "Hello World!"
-                channel.basicPublish("", QUEUE_NAME, null, message.toByteArray())
-                println(" [x] Sent '$message'")
-            }
+fun main(args: Array<String>) {
+    val connectionFactory = ConnectionFactory()
+    connectionFactory.host = "localhost"
+    connectionFactory.connectionTimeout = 3000
+    connectionFactory.newConnection().use { connection ->
+        connection.createChannel().use { channel ->
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null)
+            val message = "Hello World!"
+            channel.basicPublish("", QUEUE_NAME, null, message.toByteArray())
+            println(" [x] Sent '$message'")
         }
     }
 }
